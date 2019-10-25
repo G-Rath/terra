766b18ee3a96f1980cb23ced1abd033cffc4a91a
Gareth Jones <jones258@gmail.com>
2019-10-25 18:26:20 +1300

    Create `printBlockLiteral` printer

--- src/printers/index.ts
+++ src/printers/index.ts
@@ -2,3 +2,4 @@ export * from './printPrimitiveLiteral';
 export * from './printLiteralExpression';
 export * from './printArgument';
 export * from './printBlockBody';
+export * from './printBlockLiteral';
