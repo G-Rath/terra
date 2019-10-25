c1bb8b3fa0643a0e62fa236bdb7a6f60421c4ac0
Gareth Jones <jones258@gmail.com>
2019-10-25 19:29:24 +1300

    Use `printBlockLiteral` in `printBlockBody` printer

--- test/src/printers/printBlockBody.spec.ts
+++ test/src/printers/printBlockBody.spec.ts
@@ -22,6 +22,41 @@ describe('printBlockBody', () => {
         ).toMatchSnapshot();
       });
     });
+
+    describe('when the element is a block', () => {
+      it('prints as expected', () => {
+        expect(
+          printBlockBody([
+            {
+              type: TFNodeType.Block,
+              name: 'ingress',
+              body: [
+                {
+                  type: TFNodeType.Argument,
+                  identifier: 'from_port',
+                  expression: 0
+                },
+                {
+                  type: TFNodeType.Argument,
+                  identifier: 'to_port',
+                  expression: 0
+                },
+                {
+                  type: TFNodeType.Argument,
+                  identifier: 'protocol',
+                  expression: '"-1"'
+                },
+                {
+                  type: TFNodeType.Argument,
+                  identifier: 'security_groups',
+                  expression: ['aws_security_group.wordpress_server.id']
+                }
+              ]
+            }
+          ])
+        ).toMatchSnapshot();
+      });
+    });
   });
   describe('when the body contains multiple elements', () => {
     describe('when the elements are just arguments', () => {
@@ -56,6 +91,134 @@ describe('printBlockBody', () => {
                   ]
                 ]
               }
+            }
+          ])
+        ).toMatchSnapshot();
+      });
+    });
+
+    describe('when the elements are just blocks', () => {
+      it('prints as expected', () => {
+        expect(
+          printBlockBody([
+            {
+              type: TFNodeType.Block,
+              name: 'cookies',
+              body: [
+                {
+                  type: TFNodeType.Argument,
+                  identifier: 'forward',
+                  expression: '"none"'
+                },
+                {
+                  type: TFNodeType.Argument,
+                  identifier: 'whitelisted_names',
+                  expression: []
+                }
+              ]
+            },
+            {
+              type: TFNodeType.Block,
+              name: 'ingress',
+              body: [
+                {
+                  type: TFNodeType.Argument,
+                  identifier: 'from_port',
+                  expression: 0
+                },
+                {
+                  type: TFNodeType.Argument,
+                  identifier: 'to_port',
+                  expression: 0
+                },
+                {
+                  type: TFNodeType.Argument,
+                  identifier: 'protocol',
+                  expression: '"-1"'
+                },
+                {
+                  type: TFNodeType.Argument,
+                  identifier: 'security_groups',
+                  expression: ['aws_security_group.wordpress_server.id']
+                }
+              ]
+            }
+          ])
+        ).toMatchSnapshot();
+      });
+    });
+
+    describe('when the elements are a mix', () => {
+      it('prints as expected', () => {
+        expect(
+          printBlockBody([
+            {
+              type: TFNodeType.Argument,
+              identifier: 'allowed_methods',
+              expression: ['GET', 'HEAD']
+            },
+            {
+              type: TFNodeType.Argument,
+              identifier: 'cached_methods',
+              expression: ['GET', 'HEAD']
+            },
+            ...([
+              ['compress', true],
+              ['default_ttl', 31536000],
+              ['max_ttl', 31536000],
+              ['min_ttl', 0],
+              ['path_pattern', '"/static/*"'],
+              ['smooth_streaming', false],
+              ['target_origin_id', '"S3-app.mine/test"'],
+              ['viewer_protocol_policy', 'https-only']
+            ] as const).map(
+              ([identifier, expression]): TFArgument => ({
+                type: TFNodeType.Argument as const,
+                identifier,
+                expression
+              })
+            ),
+            {
+              type: TFNodeType.Argument,
+              identifier: 'trusted_signers',
+              expression: []
+            },
+            {
+              type: TFNodeType.Block,
+              name: 'forwarded_values',
+              body: [
+                {
+                  type: TFNodeType.Argument,
+                  identifier: 'headers',
+                  expression: []
+                },
+                {
+                  type: TFNodeType.Argument,
+                  identifier: 'query_string',
+                  expression: false
+                },
+                {
+                  type: TFNodeType.Argument,
+                  identifier: 'query_string_cache_keys',
+                  expression: []
+                },
+                {
+                  type: TFNodeType.Block,
+                  name: 'cookies',
+                  body: [
+                    {
+                      type: TFNodeType.Argument,
+                      identifier: 'forward',
+                      expression: '"none"'
+                    },
+                    {
+                      type: TFNodeType.Argument,
+                      identifier: 'whitelisted_names',
+                      expression: []
+                    }
+                  ]
+                }
+              ]
             }
           ])
         ).toMatchSnapshot();
