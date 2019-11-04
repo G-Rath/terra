66639afaf791debf665b04997f545e3c158a8987
Gareth Jones <jones258@gmail.com>
2019-11-04 18:31:53 +1300

    Create `makeTFStringArgument` utility function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,5 @@
 export * from './AwsResourceType';
 export * from './asResourceName';
 export * from './makeTFArgument';
+export * from './makeTFStringArgument';
 export * from './normaliseRoute53Name';
