65f43b454c79d81f74e81cdaaaa31567d095dad7
Gareth Jones <jones258@gmail.com>
2019-11-04 19:03:49 +1300

    Create `makeTFDynamicBlock` utility function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,5 +2,6 @@ export * from './AwsResourceType';
 export * from './asResourceName';
 export * from './makeTFArgument';
 export * from './makeTFBlockLiteral';
+export * from './makeTFDynamicBlock';
 export * from './makeTFStringArgument';
 export * from './normaliseRoute53Name';
