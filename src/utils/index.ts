4f5504e816b828ef77c0547eaf1e93c1815921cc
Gareth Jones <jones258@gmail.com>
2019-11-04 19:03:03 +1300

    Create `makeTFBlockLiteral` utility function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,6 @@
 export * from './AwsResourceType';
 export * from './asResourceName';
 export * from './makeTFArgument';
+export * from './makeTFBlockLiteral';
 export * from './makeTFStringArgument';
 export * from './normaliseRoute53Name';
