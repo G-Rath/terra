80f406baae8900c7a4321a6bfedb02617d7b1477
Gareth Jones <jones258@gmail.com>
2019-11-09 20:14:07 +1300

    Setup `bin` script using `oclif`

---
+++ src/commands/hello_world.ts
@@ -0,0 +1,27 @@
+import { Command, flags } from '@oclif/command';
+
+export default class HelloWorld extends Command {
+  static description = 'Says hello to the world';
+
+  static examples = [
+    `$ terra hello_world
+hello world from ./src/hello.ts!
+`
+  ];
+
+  static flags = {
+    name: flags.string({
+      description: 'the name of the file to create',
+      char: 'n'
+    })
+  };
+
+  static args = [];
+
+  async run() {
+    const { flags } = this.parse(HelloWorld);
+
+    const name = flags.name || 'world';
+    this.log(`hello ${name} from ${__dirname}`);
+  }
+}
---
+++ test/src/commands/hello_world.spec.ts
@@ -0,0 +1,41 @@
+import HelloWorld from '@src/commands/hello_world';
+
+describe('hello_world', () => {
+  let stdoutSpy = jest.spyOn(process.stdout, 'write');
+
+  beforeEach(() => (stdoutSpy = jest.spyOn(process.stdout, 'write')));
+
+  it('should say hello', async () => {
+    await HelloWorld.run([]);
+
+    expect(stdoutSpy).toHaveBeenCalledWith(expect.stringContaining('hello'));
+  });
+
+  describe('the --name flag', () => {
+    it('should use the name when provided', async () => {
+      await HelloWorld.run(['--name', 'person']);
+
+      expect(stdoutSpy).toHaveBeenCalledWith(
+        expect.stringContaining('hello person')
+      );
+    });
+
+    it('should map -n to --name', async () => {
+      await HelloWorld.run(['-n', 'sunshine']);
+
+      expect(stdoutSpy).toHaveBeenCalledWith(
+        expect.stringContaining('hello sunshine')
+      );
+    });
+
+    describe('when the name flag is omitted', () => {
+      it('should default to "world"', async () => {
+        await HelloWorld.run([]);
+
+        expect(stdoutSpy).toHaveBeenCalledWith(
+          expect.stringContaining('hello world')
+        );
+      });
+    });
+  });
+});
