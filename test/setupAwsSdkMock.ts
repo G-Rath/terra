44e58b7eada6f46c8455408804bbde01548ff8e6
Gareth Jones <jones258@gmail.com>
2019-11-13 18:37:20 +1300

    Create `setupAwsSdkMock` setup file & client

---
+++ test/setupAwsSdkMock.ts
@@ -0,0 +1,36 @@
+type AWSClientMap = typeof import('aws-sdk/clients/all');
+
+/** Map of `aws-sdk` clients to their mocked endpoints */
+const clientMocks: { [K in keyof AWSClientMap]?: unknown } = {};
+
+export const mockAwsClientEndpoints = <
+  TClient extends keyof AWSClientMap,
+  TEndpoint extends keyof InstanceType<AWSClientMap[TClient]>,
+  TMockedEndpoints extends Partial<Record<TEndpoint, jest.Mock>>
+>(
+  client: TClient,
+  mockedEndpoints: TMockedEndpoints
+): TMockedEndpoints => {
+  const clientProxy = new Proxy(class {}, {
+    construct(): object {
+      return clientProxy;
+    },
+    get(t, property: TEndpoint) {
+      const endpointMock = mockedEndpoints[property];
+
+      if (endpointMock) {
+        return (...args: unknown[]) => ({
+          promise: () => endpointMock(...args)
+        });
+      }
+
+      throw new Error(`${client}#${property} was called without being mocked`);
+    }
+  });
+
+  clientMocks[client] = clientProxy;
+
+  return mockedEndpoints;
+};
+
+jest.mock('aws-sdk', () => clientMocks);
