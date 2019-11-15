8b881926c9afa567cb26a0fa82c53aac3a368a4f
Gareth Jones <jones258@gmail.com>
2019-11-15 18:49:02 +1300

    Create `AwsRoute53Zone` command

---
+++ src/commands/aws_route53_zone.ts
@@ -0,0 +1,48 @@
+import { Command, flags } from '@oclif/command';
+import { nadoRoute53Zone } from '@src/nados/aws';
+import { TFNodeType } from '@src/types';
+import { AwsResourceType } from '@src/utils';
+
+export default class AwsRoute53Zone extends Command {
+  static description = 'Generates Terraform configuration for a Route53 Zone';
+
+  static examples = [
+    `$ terra aws_route53_zone /HostedZone/123456789
+hello world from ./src/hello.ts!
+`
+  ];
+
+  static flags = {
+    greedy: flags.boolean({
+      description:
+        'if true, will greedily nado configuration for related resources, such as records',
+      default: false,
+      char: 'g'
+    })
+  };
+
+  static args = [{ name: 'zoneId' }];
+
+  async run() {
+    const {
+      args: { zoneId },
+      flags: { greedy }
+    } = this.parse(AwsRoute53Zone);
+
+    const tfRoot = await nadoRoute53Zone(zoneId, greedy);
+
+    const zoneResource = tfRoot.find(
+      block =>
+        block.type === TFNodeType.Resource &&
+        block.resource === AwsResourceType.AWS_ROUTE53_ZONE
+    );
+
+    if (!zoneResource) {
+      throw new Error('assertion failure: "zoneResource" cannot be undefined');
+    }
+
+    const fileName = `route_${zoneResource.name}`;
+
+    console.log(fileName);
+  }
+}
---
+++ test/src/commands/aws_route53_zone.spec.ts
@@ -0,0 +1,63 @@
+import AwsRoute53Zone from '@src/commands/aws_route53_zone';
+import { nadoRoute53Zone } from '@src/nados';
+import { AwsResourceType, makeTFResourceBlock } from '@src/utils';
+import { mocked } from 'ts-jest/utils';
+
+jest.mock('@src/nados/aws/nadoRoute53Zone');
+
+const nadoRoute53ZoneMock = mocked(nadoRoute53Zone);
+
+describe('aws_route53_zone', () => {
+  const zoneId = '/HostedZone/123456789';
+
+  let stdoutSpy = jest.spyOn(process.stdout, 'write');
+
+  beforeEach(() => (stdoutSpy = jest.spyOn(process.stdout, 'write')));
+  beforeEach(() =>
+    nadoRoute53ZoneMock.mockResolvedValue([
+      makeTFResourceBlock('my_zone_com', AwsResourceType.AWS_ROUTE53_ZONE, [])
+    ])
+  );
+
+  it('should pass the zoneId to nadoRoute53Zone', async () => {
+    await AwsRoute53Zone.run([zoneId]);
+
+    expect(nadoRoute53ZoneMock).toHaveBeenCalledWith(
+      zoneId,
+      expect.any(Boolean)
+    );
+  });
+
+  describe('the --greedy flag', () => {
+    it('calls nadoRoute53Zone with greed', async () => {
+      await AwsRoute53Zone.run(['--greedy', zoneId]);
+
+      expect(nadoRoute53ZoneMock).toHaveBeenCalledWith(zoneId, true);
+    });
+
+    it('should map -g to --greedy', async () => {
+      await AwsRoute53Zone.run(['-g', zoneId]);
+
+      expect(nadoRoute53ZoneMock).toHaveBeenCalledWith(zoneId, true);
+    });
+
+    describe('when omitted', () => {
+      it('defaults to "false"', async () => {
+        await AwsRoute53Zone.run([zoneId]);
+
+        expect(nadoRoute53ZoneMock).toHaveBeenCalledWith(zoneId, false);
+      });
+    });
+  });
+
+  describe('the output', () => {
+    it('prints the file name', async () => {
+      await AwsRoute53Zone.run([zoneId]);
+
+      expect(stdoutSpy).toHaveBeenCalledWith(
+        expect.stringContaining('route_my_zone_com'),
+        expect.anything()
+      );
+    });
+  });
+});
