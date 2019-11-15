16ae64dbbe476519ce60a95cd6276904d8247abc
Gareth Jones <jones258@gmail.com>
2019-11-15 16:16:49 +1300

    Create `makeTFResourceBlock` utility function

---
+++ src/utils/makeTFResourceBlock.ts
@@ -0,0 +1,25 @@
+import { TFRoute53RecordResource, TFRoute53ZoneResource } from '@src/builders';
+import {
+  ResourceType,
+  TFBlockBody,
+  TFNodeType,
+  TFResourceBlock
+} from '@src/types';
+import { AwsResourceType } from '@src/utils/AwsResourceType';
+
+interface ResourceIdentifierMap {
+  [AwsResourceType.AWS_ROUTE53_RECORD]: keyof TFRoute53RecordResource;
+  [AwsResourceType.AWS_ROUTE53_ZONE]: keyof TFRoute53ZoneResource;
+  [k: string]: string;
+}
+
+export const makeTFResourceBlock = <TResource extends ResourceType>(
+  name: string,
+  resource: TResource,
+  body: TFBlockBody<ResourceIdentifierMap[TResource]>
+): TFResourceBlock<ResourceIdentifierMap[TResource]> => ({
+  type: TFNodeType.Resource,
+  name,
+  resource,
+  body
+});
