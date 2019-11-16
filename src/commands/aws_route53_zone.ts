a82b4111aff32406d41be68c2455197f2100278a
Gareth Jones <jones258@gmail.com>
2019-11-17 10:29:10 +1300

    chore(aws_route53_zone): adjust zoneId in example to reflect aws cli output

--- src/commands/aws_route53_zone.ts
+++ src/commands/aws_route53_zone.ts
@@ -7,7 +7,7 @@ export default class AwsRoute53Zone extends Command {
   static description = 'Generates Terraform configuration for a Route53 Zone';
 
   static examples = [
-    `$ terraport aws_route53_zone /HostedZone/123456789
+    `$ terraport aws_route53_zone /hostedzone/123456789
 hello world from ./src/hello.ts!
 `
   ];
