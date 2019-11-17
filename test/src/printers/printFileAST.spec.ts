cab3f6dbbf421a6f1cd585c5d3593cba1b99770c
Gareth Jones <jones258@gmail.com>
2019-11-17 13:22:55 +1300

    chore(printers): create `printFileAST` for printing `TFFileAST`

---
+++ test/src/printers/printFileAST.spec.ts
@@ -0,0 +1,40 @@
+import { printFileAST, printResourceBlock } from '@src/printers';
+import { TFNodeType } from '@src/types';
+import { makeTFResourceBlock } from '@src/utils';
+import { mocked } from 'ts-jest/utils';
+
+jest.mock('@src/printers/printResourceBlock');
+
+const printResourceBlockMock = mocked(printResourceBlock);
+
+describe('printFileAST', () => {
+  describe('when the AST contains a resource block', () => {
+    it('uses printResourceBlock', () => {
+      const resource = makeTFResourceBlock('my-resource', 'my-type', []);
+
+      printFileAST([resource]);
+
+      expect(printResourceBlockMock).toHaveBeenCalledWith(resource);
+    });
+  });
+
+  describe('when the AST is empty', () => {
+    it('prints nothing', () => {
+      expect(printFileAST([])).toBe('');
+    });
+  });
+
+  describe('when the block type is not supported', () => {
+    it('throws an error', () => {
+      expect(() =>
+        printFileAST([
+          {
+            type: TFNodeType.Module,
+            name: 'my module',
+            body: []
+          }
+        ])
+      ).toThrow(`"${TFNodeType.Module}" is not yet supported`);
+    });
+  });
+});
