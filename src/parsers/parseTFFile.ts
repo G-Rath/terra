55895a195629268aa8216c1d2faf77515c816a0b
Gareth Jones <jones258@gmail.com>
2019-12-15 22:29:19 +1300

    chore: remove old parsers

--- src/parsers/parseTFFile.ts
+++
@@ -1,8 +0,0 @@
-import { parseTFFileContents } from '@src/parsers';
-import * as fs from 'fs';
-
-export const parseTFFile = (filePath: string) => {
-  const fileContents = fs.readFileSync(filePath).toString();
-
-  return parseTFFileContents(fileContents);
-};
