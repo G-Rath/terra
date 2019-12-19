ddc9de786cfe4da02546ecbd619d2d0bf6228257
Gareth Jones <jones258@gmail.com>
2019-12-19 18:41:26 +1300

    refactor(TFModuleBlock): extend `TFBlock`

--- src/printers/printFileAST.ts
+++ src/printers/printFileAST.ts
@@ -1,14 +1,5 @@
 import { printTFBlock } from '@src/printer';
-import { TFFileAST, TFNodeType } from '@src/types';
+import { TFFileAST } from '@src/types';
 
 export const printFileAST = (fileAST: TFFileAST) =>
-  fileAST
-    .map(block => {
-      switch (block.type) {
-        case TFNodeType.Block:
-          return printTFBlock(block);
-        default:
-          throw new Error(`"${block.type}" is not yet supported`);
-      }
-    })
-    .join('\n\n');
+  fileAST.map(printTFBlock).join('\n\n');
