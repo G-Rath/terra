b8cba2a601ffbbf987e3bba3502e98548f446e37
Gareth Jones <jones258@gmail.com>
2019-12-23 23:45:47 +1300

    fix(printTFBlocks): don't add artificial newlines when joining

--- src/printer/printTFBlocks.ts
+++ src/printer/printTFBlocks.ts
@@ -2,4 +2,4 @@ import { printTFBlock } from '@src/printer';
 import { TFBlock } from '@src/types';
 
 export const printTFBlocks = (blocks: TFBlock[]) =>
-  blocks.map(printTFBlock).join('\n\n');
+  blocks.map(printTFBlock).join('');
