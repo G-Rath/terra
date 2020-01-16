0ee21d2c0cd2afcd6705fce21af092574ea6aa7e
Gareth Jones <jones258@gmail.com>
2020-01-16 17:17:43 +1300

    feat: create `format` function

---
+++ test/src/formatter/format.spec.ts
@@ -0,0 +1,78 @@
+import { format } from '@src/formatter';
+import { makeTFResourceBlock } from '@src/makers';
+import { TFFileContents } from '@src/types';
+import { AwsResourceType } from '@src/utils';
+
+describe('format', () => {
+  describe('when blocks is empty', () => {
+    it('does nothing', () => {
+      expect(
+        format({
+          blocks: [],
+          surroundingText: {
+            leadingOuterText: '',
+            trailingOuterText: ''
+          }
+        })
+      ).toStrictEqual<TFFileContents>({
+        blocks: [],
+        surroundingText: {
+          leadingOuterText: '',
+          trailingOuterText: ''
+        }
+      });
+    });
+  });
+
+  describe('when there is at least one block', () => {
+    it('ensures the file ends with a new line', () => {
+      expect(
+        format({
+          blocks: [
+            makeTFResourceBlock(
+              AwsResourceType.AWS_ROUTE53_ZONE, //
+              'my_zone',
+              []
+            )
+          ],
+          surroundingText: {
+            leadingOuterText: '',
+            trailingOuterText: ''
+          }
+        })
+      ).toStrictEqual<TFFileContents>({
+        blocks: [],
+        surroundingText: {
+          leadingOuterText: '',
+          trailingOuterText: '\n'
+        }
+      });
+    });
+
+    describe('when the file already ends with a newline', () => {
+      it('does not add another', () => {
+        expect(
+          format({
+            blocks: [
+              makeTFResourceBlock(
+                AwsResourceType.AWS_ROUTE53_ZONE,
+                'my_zone',
+                []
+              )
+            ],
+            surroundingText: {
+              leadingOuterText: '',
+              trailingOuterText: '\n'
+            }
+          })
+        ).toStrictEqual<TFFileContents>({
+          blocks: [],
+          surroundingText: {
+            leadingOuterText: '',
+            trailingOuterText: '\n'
+          }
+        });
+      });
+    });
+  });
+});
