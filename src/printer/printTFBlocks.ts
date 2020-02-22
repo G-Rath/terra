1b05a88e30595e2ec77369bbd62da212b0810ca8
Gareth Jones <jones258@gmail.com>
2020-02-22 16:34:37 +1300

    chore(printer): denote return type of `printTFBlocks` explicitly

--- src/printer/printTFBlocks.ts
+++ src/printer/printTFBlocks.ts
@@ -1,5 +1,5 @@
 import { printTFBlock } from '@src/printer';
 import { TFBlock } from '@src/types';
 
-export const printTFBlocks = (blocks: TFBlock[]) =>
+export const printTFBlocks = (blocks: TFBlock[]): string =>
   blocks.map(printTFBlock).join('');
