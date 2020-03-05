7130fa18ec2f0fdd37d3a5fc47f96512550eb146
Gareth Jones <jones258@gmail.com>
2020-03-06 09:14:16 +1300

    feat(formatter): create `ensureNoSpacesAfterFunctionName` ensurer

--- src/formatter/ensurers.ts
+++ src/formatter/ensurers.ts
@@ -116,8 +116,36 @@ export const ensureSpaceBeforeOpeningBrace: Ensurer = blocks => {
   });
 };
 
+export const ensureNoSpacesAfterFunctionName: Ensurer = blocks =>
+  walkNodes(blocks, {
+    Function: ({ name, surroundingText }) => {
+      surroundingText.leadingOuterText = surroundingText.leadingOuterText.trim();
+      name.surroundingText.trailingOuterText = name.surroundingText.trailingOuterText.trim();
+
+      if (surroundingText.leadingOuterText.length) {
+        const parsedText = parseSurroundingText(
+          surroundingText.leadingOuterText
+        );
+
+        const lastToken = parsedText[parsedText.length - 1];
+
+        if (
+          lastToken.type !== TokenType.Comment ||
+          lastToken.content.startsWith('/*')
+        ) {
+          return;
+        }
+
+        surroundingText.leadingOuterText = printTokens(
+          parsedText.concat({ type: TokenType.Newline })
+        );
+      }
+    }
+  });
+
 export const ensurers: readonly Ensurer[] = Object.freeze([
   ensureTopLevelBlocksAreSeparated,
+  ensureNoSpacesAfterFunctionName,
   ensureSpaceBeforeOpeningBrace,
   ensureLabelsHaveLeadingSpace,
   ensureClosingBraceOnNewline
